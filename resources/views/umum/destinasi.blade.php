9addd520afe80084d7a69b6e63c57c5b103ec940
Romdhoni17 <nromdhoni17@gmail.com>
2022-05-18 18:15:02 +0700

    landing page

---
+++ resources/views/umum/destinasi.blade.php
@@ -0,0 +1,1 @@
+Ini destinasi Arul ganteng
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('umum.home');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('/destinasi', function () {
+    return view('umum.destinasi');
+});
+
+Route::get('/about', function () {
+    return view('umum.about');
+});
+
+Route::get('/kontak', function () {
+    return view('umum.kontak');
+});
